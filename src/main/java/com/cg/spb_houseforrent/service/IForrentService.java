d7dded97c9c5620db643c6810c350064b6cae51a
QuyDang1209 <dangphuocquy1996@gmail.com>
2024-07-12 16:46:01 +0700

    booking

--- src/main/java/com/cg/spb_houseforrent/service/IForrentService.java
+++ src/main/java/com/cg/spb_houseforrent/service/IForrentService.java
@@ -1,13 +1,17 @@
 package com.cg.spb_houseforrent.service;
 
 import com.cg.spb_houseforrent.model.Forrent;
+import com.cg.spb_houseforrent.model.dto.FilterForrent;
 import com.cg.spb_houseforrent.model.dto.ForrentDTO;
 import com.cg.spb_houseforrent.model.dto.res.ForrentResDTO;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 public interface IForrentService extends IGenericService<Forrent> {
     Forrent saveForrentDto(ForrentDTO forrentDTO);
 
     List<ForrentResDTO> findAllForrentDTO();
+    Set<ForrentResDTO> findAllForrentDTO(FilterForrent filterForrent);
 }
